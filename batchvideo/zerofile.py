9dfbd7f86a72a0c7e0bbc44d885f07fcd6cbd7e7
CcccFz <liuxaiofan39@gmail.com>
2018-02-05 11:47:56 +0800

    修改zerofile

--- batchvideo/zerofile.py
+++ batchvideo/zerofile.py
@@ -3,9 +3,9 @@ import os
 
 base_path = os.path.dirname(__file__)
 for curdir, dirnames, filenames in os.walk('videos'):
-	dir_path = os.path.join(base_path, curdir)
-	for filename in filenames:
-		file_path = os.path.join(dir_path, filename)
-		if os.path.getsize(file_path) == 0:
-			print(file_path)
-			os.remove(file_path)
+    dir_path = os.path.join(base_path, curdir)
+    for filename in filenames:
+        file_path = os.path.join(dir_path, filename)
+        if os.path.getsize(file_path) == 0:
+            print(file_path)
+            os.remove(file_path)
